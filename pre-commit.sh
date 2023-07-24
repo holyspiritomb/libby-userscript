98e2692320c8e9c3e40e88703d47684fde57ae6f
Hezekiah Michael <spiritomb@protonmail.com>
2023-07-24 18:31:53 -0400

    update precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+git add goodreads-libby.user.js
 cat goodreads-libby.user.js | sed "s/@grant         GM_addStyle/@grant         GM_addStyle\n\/\/ @grant         unsafeWindow/;s/main\/goodreads-libby.user.js/main\/goodreads-libby-adguard.user.js/" > goodreads-libby-adguard.user.js
+
+git add goodreads-libby-adguard.user.js
