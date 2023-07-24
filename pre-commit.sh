62336016184757ad7810ed68272290c1ccc077d7
Hezekiah Michael <spiritomb@protonmail.com>
2023-07-24 18:26:55 -0400

    add precommit to automate adguard script

---
+++ pre-commit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat goodreads-libby.user.js | sed "s/@grant         GM_addStyle/@grant         GM_addStyle\n\/\/ @grant         unsafeWindow/;s/main\/goodreads-libby.user.js/main\/goodreads-libby-adguard.user.js/" > goodreads-libby-adguard.user.js
